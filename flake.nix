f318523dbd28dccec7aad795a3eacfcc7c78d8d4
Kcraft059 <kcraft059.msg@gmail.com>
2026-01-09 11:23:00 +0100

    various venv support

--- flake.nix
+++ flake.nix
@@ -35,6 +35,76 @@
 
       effectivePkgsX86 = if pkgsX86 != null then pkgsX86 else pkgs;
 
+      pyShellHook =
+        python3env: venv-config:
+        let
+          pipkgs-format = pkgs.lib.concatStringsSep "\n" venv-config.pipkgs;
+
+          venv-name = ".venv_" + venv-config.name;
+        in
+        ''
+          cat > .requirements.txt <<EOF
+          ${pipkgs-format}
+          EOF
+
+          cat > .zsh-shell <<'EOF'
+          export spmt='\033[38;5;5;1m> \033[0m\033[48;5;0m' # >_
+          export sqes='\033[48;5;0;38;5;8m[\033[38;5;4m?\033[38;5;8m]\033[0m' # [?]
+          export scac='\033[48;5;0;38;5;8m[\033[38;5;1mx\033[38;5;8m]\033[0m' # [x]
+          export sexc='\033[48;5;0;38;5;8m[\033[38;5;2mo\033[38;5;8m]\033[0m' # [o]
+          export smak='\033[48;5;0;38;5;8m[\033[38;5;5m+\033[38;5;8m]\033[0m' # [+]
+          export swrn='\033[48;5;0;38;5;8m[\033[38;5;3m!\033[38;5;8m]\033[0m' # [!]
+          export syon='(\033[38;5;2;1my\033[38;5;0m/\033[38;5;1;1mn\033[0m)' # (y/n)
+
+          for file in $(find ./ -name '.venv*'); do
+            if [[ $file != ./${venv-name} ]]; then
+              echo "$scac Removing other venv $file"
+              rm -rf $file
+            fi
+          done
+
+          # If venv is outdated, recreate
+          if [[ ! -f "$(readlink -f ./${venv-name}/bin/python)" ]] && [[ -d ./${venv-name} ]]; then 
+            echo -e "$swrn Removing outdated venv"
+            rm -rf ./${venv-name}/
+          fi 
+
+          # Create venv if not present
+          [[ -d ./${venv-name} ]] || ( python -m venv ./${venv-name} && echo -e "$smak Created venv ./${venv-name}" )
+
+          pydir=$(ls ${python3env}/lib/ | grep "^python")
+
+          # Clean old links
+          for file in $(find ./${venv-name}/lib/''${pydir}/site-packages -maxdepth 1 -type l); do # Find all links in site-packages
+            target="$(readlink "$file")" # If target file is not in current derivation, delete
+            if ! echo $target | grep "${python3env}" >/dev/null;then 
+              rm -rf $file
+            fi
+          done
+          echo -e "$swrn Cleaned old dependencies"
+
+          # Links dependencies
+          for file in ${python3env}/lib/''${pydir}/site-packages/*; do
+            ln -s $file ./${venv-name}/lib/''${pydir}/site-packages/ 1>/dev/null 2>&1 
+          done
+          echo -e "$smak Linked site-packages"
+
+          # Activate
+          source ./${venv-name}/bin/activate
+          echo -e "$sexc Activated venv"
+          pip install -r .requirements.txt
+          echo -e "$smak Installed requirements"
+
+          # On exit deactivate
+          TRAPEXIT() { 
+            echo -e "$scac Deactivated venv"
+            rm -rf .zsh-shell .requirements.txt
+            deactivate
+          }
+          EOF
+
+          exec ${pkgs.zsh}/bin/zsh --rcs -i -c "source .zsh-shell; ${pkgs.zsh}/bin/zsh -i"
+        '';
       /*
         # Configure pypkgs override
         packageOverrides = pkgs.callPackage ./python-packages.nix { };
@@ -44,84 +114,53 @@
     {
       # Maybe ditch flake.nix for devenv.nix see https://devenv.sh
 
-      devShells."${system}".default =
-        let
-          venv-packages = pkgs.lib.concatStringsSep "\n" [
-            #"matplotlib==3.10.7"
-            "pillow==12.0.0"
-          ];
+      devShells."${system}" = {
+        default =
+          let
+            venv-packages = [ ];
 
-          python3env = pkgs.python3.withPackages (
-            p: with p; [
-              pip
-              tkinter
-            ]
-          );
-        in
-        pkgs.mkShell {
-          packages = [
-            python3env
-            # pyxel # for pyxel no python import
-          ];
+            python3env = pkgs.python3.withPackages (
+              p: with p; [
+                pip
+              ]
+            );
+          in
+          pkgs.mkShell {
+            packages = [
+              python3env
+            ];
 
-          shellHook = ''
-            cat > .requirements.txt <<EOF
-            ${venv-packages}
-            EOF
+            shellHook = pyShellHook python3env {
+              pipkgs = venv-packages;
+              name = "default";
+            };
+          };
 
-            cat > .zsh-shell <<'EOF'
-            export spmt='\033[38;5;5;1m> \033[0m\033[48;5;0m' # >_
-            export sqes='\033[48;5;0;38;5;8m[\033[38;5;4m?\033[38;5;8m]\033[0m' # [?]
-            export scac='\033[48;5;0;38;5;8m[\033[38;5;1mx\033[38;5;8m]\033[0m' # [x]
-            export sexc='\033[48;5;0;38;5;8m[\033[38;5;2mo\033[38;5;8m]\033[0m' # [o]
-            export smak='\033[48;5;0;38;5;8m[\033[38;5;5m+\033[38;5;8m]\033[0m' # [+]
-            export swrn='\033[48;5;0;38;5;8m[\033[38;5;3m!\033[38;5;8m]\033[0m' # [!]
-            export syon='(\033[38;5;2;1my\033[38;5;0m/\033[38;5;1;1mn\033[0m)' # (y/n)
+        pillow =
+          let
+            venv-packages = [
+              #"matplotlib==3.10.7"
+              "pillow==12.0.0"
+            ];
 
-            # If venv is outdated, recreate
-            if [[ ! -f "$(readlink -f ./.venv/bin/python)" ]] && [[ -d ./.venv ]]; then 
-              echo -e "$swrn Removing outdated venv"
-              rm -rf ./.venv/
-            fi 
+            python3env = pkgs.python3.withPackages (
+              p: with p; [
+                pip
+                tkinter
+              ]
+            );
+          in
+          pkgs.mkShell {
+            packages = [
+              python3env
+              # pyxel # for pyxel no python import
+            ];
 
-            mkdir -p ./.vscode/extensions/ 
-
-            # Create venv if not present
-            [[ -d ./.venv ]] || ( python -m venv .venv && echo -e "$smak Created venv" )
-
-            pydir=$(ls ${python3env}/lib/ | grep "^python")
-
-            # Clean old links
-            for file in $(find .venv/lib/''${pydir}/site-packages -maxdepth 1 -type l); do # Find all links in site-packages
-              target="$(readlink "$file")" # If target file is not in current derivation, delete
-              if ! echo $target | grep "${python3env}" >/dev/null;then 
-                rm -rf $file
-              fi
-            done
-            echo -e "$swrn Cleaned old dependencies"
-
-            # Links dependencies
-            for file in ${python3env}/lib/''${pydir}/site-packages/*; do
-              ln -s $file ./.venv/lib/''${pydir}/site-packages/ 1>/dev/null 2>&1 
-            done
-            echo -e "$smak Linked site-packages"
-
-            # Activate
-            source ./.venv/bin/activate
-            echo -e "$sexc Activated venv"
-            pip install -r .requirements.txt
-            echo -e "$smak Installed requirements"
-
-            # On exit deactivate
-            TRAPEXIT() { 
-              echo -e "$scac Deactivated venv"
-              rm -rf .zsh-shell .requirements.txt
-              deactivate
-            }
-            EOF
-
-            exec ${pkgs.zsh}/bin/zsh --rcs -i -c "source .zsh-shell; ${pkgs.zsh}/bin/zsh -i"
-          '';
-        };
+            shellHook = pyShellHook python3env {
+              pipkgs = venv-packages;
+              name = "pillow";
+            };
+          };
+      };
     };
 }
--- isn/photo-filter/photo-filter.py
+++ isn/photo-filter/photo-filter.py
@@ -1,97 +1,113 @@
 import PIL.Image as pimg
-from helplib import helpLib 
+from helplib import helpLib # Récupère les entrées du manuel depuis helplib.py
 
 currentImg = None
 
 ## Commandes du programme
 def printHelp(args) :
-  helpKeyword = args[0] if len(args) > 0 else None
+  """Affiche une entrée manuel pour un mot donné"""
+  helpKeyword = args[0] if len(args) > 0 else None # Si un mot est donné en argument sinon None
 
-  if helpKeyword != None and helpKeyword in helpLib.keys():
-    print(f'Resultats pour l\'entrée de manuel "{helpKeyword}":')
+  if helpKeyword != None and helpKeyword in helpLib.keys(): # Cherche le mot dans la librairie
+    print(f'Resultat pour l\'entrée de manuel "{helpKeyword}":')
     helpmsg = helpLib[helpKeyword]
   else :
-    if helpKeyword != None and not(helpKeyword in helpLib.keys()):
+    if helpKeyword != None:
       print(f'Pas d\'entré de manuel pour "{helpKeyword}"')
-    helpmsg = helpLib["all"]
+    helpmsg = helpLib["all"] # par défaut, si aucun mot n'est donné/trouvé, afficher l'aide genérale
 
   print(helpmsg)
 
 def loadImg(args) :
+  """Charge une image dans le programme"""
   global currentImg
 
   if len(args) < 1 or args[0] == "":
-    print("Pas assez d'arguments (`load <img_path>`).")
-    return
+    raise Exception("Pas assez d'arguments (`load <img_path>`).")
 
-  clearImg(None)
+  clearImg(None) # Efface l'image chargée précédemment
 
   imgPath = args[0]
-  currentImg = pimg.open(imgPath)
+  currentImg = pimg.open(imgPath) # Charge l'image dans le programme
   print(f'Image chargée : {imgPath}')
 
 def showImg(args) :
+  """Affiche une image"""
   global currentImg
 
   if currentImg == None:
-    print("Pas d'image chargée.")
-    return
+    raise Exception("Pas d'image chargée.")
 
   print("L'image va s'ouvrir...")
   currentImg.show()
 
 def clearImg(args) :
+  """Efface l'image chargée dans le programme"""
   global currentImg;
+
   if currentImg == None: 
-    print("Pas d'image a effacer.")
-    return
+    return 
+
   currentImg.close()
   currentImg = None;
   print("Image effacée.")
 
 def fileSave(args) : 
+  """Enregistre l'image chargée dans le programme"""
   global currentImg
+
   if len(args) < 1 or args[0] == "":
-    print("Pas assez d'arguments (`save <img_name>`).")
-    return
+    raise Exception("Pas assez d'arguments (`save <img_name>`).")
   if currentImg == None: 
-    print("Aucune image chargée.")
-    return
+    raise Exception("Aucune image chargée.")
+
   currentImg.save(args[0])
+
   print(f'Image enregistrée en tant que "{args[0]}".')
 
 def printCredit(args) :
+  """Affiche l'auteur du projet"""
   print("""=== Projet filtre NSI === 
 - Camille Therey""")
 
 def pickColor(args) :
+  """Récupère la valeur d'un pixel de l'image chargée"""
   global currentImg
+
   argc = len(args)
   if argc < 1 or args[0] == "":
-    print("Pas assez d'arguments (`pick <position>` voir `help`).")
+    raise Exception("Pas assez d'arguments (`pick <position>` voir `help`).")
     return
   if currentImg == None: 
-    print("Aucune image chargée.")
+    raise Exception("Aucune image chargée.")
     return
 
-  pos = tuple(int(i) for i in args[0].split(",")[:2])
-  values = currentImg.getpixel(pos)[:3]
+  pos = tuple(int(i) for i in args[0].split(",")[:2]) # Recupère les coordonnées dans un tuple
+
+  (w,h) = currentImg.size
+
+  if not(pos[0] >= 0 and pos[0] < w) or not(pos[1] >= 0 and pos[1] < h) : # Verifie si les coordonnées sont correctes
+     raise Exception(f"Coordonnées invalides (0<=x<{w} 0<=x<{h})!")
+
+  values = currentImg.getpixel(pos)[:3] # Recupère la valeur du pixel
 
   hexvalue = "#"
   for value in values:
-    hexvalue += str(hex(value))[2:]
+    hexvalue += str(hex(value))[2:] # Récupère la deuxième partie du hex 0xFF -> FF pour chaque canal RBG
 
-  print(f'Value of pixel {pos} is {values} or {hexvalue}.')
+  print(f'Value of pixel {pos} is rgb: {values} or hex: {hexvalue}.')
 
 def filterApply(args) :
+  """Applique une filtre sur l'image chargée"""
   global currentImg
   argc = len(args)
+  
   if argc < 1 or args[0] == "":
-    print("Pas assez d'arguments (`filter <filter_name> <?options>` voir `help filters`).")
-    return
+    raise Exception("Pas assez d'arguments (`filter <filter_name> <?options>` voir `help filters`).")
 
   currentFilter = args[0]
 
+  ## Choisi le filtre et verifie si tous les arguments sont présents
   if currentFilter == "exposure" and argc == 2 :
     currentImg = imageExposure(currentImg, int(args[1]))
   elif currentFilter == "tint" and argc >= 2 :
@@ -112,14 +128,13 @@ def filterApply(args) :
     currentImg = imageColorReplace(currentImg,args[1],int(args[2]) if argc > 2 else None)
 
   else :
-    print(f'Filtre inconnu \"{currentFilter}\" ou avec trop peu d\'arguments.')
+    raise Exception(f'Filtre inconnu \"{currentFilter}\" ou avec trop peu d\'arguments.')
     return
 
 ## Filtres
 def imageExposure(img,coeff) :
-  if img == None: 
-    print("Aucune image chargée.")
-    return img
+  """Modifie l'exposition d'une image"""
+  imgCheck(img)
 
   def rbgcoeffadd(coords, values, newimg):
     (r,g,b) = values[:3] # transparence
@@ -132,41 +147,42 @@ def imageExposure(img,coeff) :
   return newimg
 
 def imageTint(img,color,method) :
-  if img == None: 
-    print("Aucune image chargée.")
-    return img
+  """Teinte une image avec une couleur"""
+  imgCheck(img)
 
-  (mr,mg,mb) = getColor(color)
+  if method == None: 
+    method = "tint"
 
+  (mr,mg,mb) = getColor(color) # Enregistre les valeurs de la teinte
+
+  ## Choisi la methode de coloration en fonction de la methode donnée en argument
   if method == "monotone" :
     def colortint(coords, values, newimg):
       (r,g,b) = values[:3]
       avrg = (r+g+b) // 3
       newimg.putpixel(coords, ((avrg*mr)//255,(avrg*mg)//255,(avrg*mb)//255))
-  elif method == "tint" or method == None :
+
+  elif method == "tint" :
     def colortint(coords, values, newimg):
       (r,g,b) = values[:3]
       newimg.putpixel(coords, ((r*mr)//255,(g*mg)//255,(b*mb)//255))
+
   else :
-    print(f'Aucune methode "{method}" trouvée. (`help filter_tint`)')
+    raise Exception(f'Aucune methode "{method}" trouvée. (`help filter_tint`)')
     return img
 
-  newimg = perPixelAct(img,colortint)
+  newimg = perPixelAct(img,colortint) # Applique la fonction de modification sur chaque pixel de l'image
   print(f'Filtre appliqué avec la methode {method}.')
   
   img.close()
   return newimg
 
 def imageGrayscale(img) :
-  if img == None: 
-    print("Aucune image chargée.")
-    return img
+  imgCheck(img)
   return imageTint(img,"#ffffff","monotone")
 
 def imageBlackAndWhite(img) :
-  if img == None: 
-    print("Aucune image chargée.")
-    return img
+  imgCheck(img)
 
   def blackWhite(coords, values, newimg):
       (r,g,b) = values[:3]
@@ -180,22 +196,21 @@ def imageBlackAndWhite(img) :
   return newimg
 
 def imageMirror(img,axis) :
-  if img == None: 
-    print("Aucune image chargée.")
-    return img
+  imgCheck(img)
 
   (w,h) = img.size
   if axis == "x" :
     def mirror(coords, values, newimg):
       coords = (w - 1 - coords[0],coords[1])
       newimg.putpixel(coords , values)
+
   elif axis == "y" :
     def mirror(coords, values, newimg):
       coords = (coords[0],h - 1 - coords[1])
       newimg.putpixel(coords , values)
+
   else :
-    print(f'Axe inconnu "{axis}".')
-    return img
+    raise Exception(f'Axe inconnu "{axis}".')
 
   newimg = perPixelAct(img,mirror)
   print(f'Filtre appliqué.')
@@ -204,9 +219,7 @@ def imageMirror(img,axis) :
   return newimg
 
 def imageReduce(img,valueRange) :
-  if img == None: 
-    print("Aucune image chargée.")
-    return img
+  imgCheck(img)
 
   def rdc(coords, values, newimg):
       values = tuple((i*valueRange//255)*(255//valueRange) for i in values[:3])
@@ -219,9 +232,7 @@ def imageReduce(img,valueRange) :
   return newimg
 
 def imagePixel(img,pixels) :
-  if img == None: 
-    print("Aucune image chargée.")
-    return img
+  imgCheck(img)
 
   (w,h) = img.size
   newimg = pimg.new("RGB",tuple(i//pixels for i in img.size))
@@ -248,17 +259,15 @@ def imagePixel(img,pixels) :
   return newimg
 
 def imageSwap(img,primaryValues) :
-  if img == None: 
-    print("Aucune image chargée.")
-    return img
+  imgCheck(img)
 
   primaryValues = tuple(0 if i == "r" else 1 if i == "g" else 2 for i in primaryValues.split("/")[:2])
 
   def swap(coords,values,newimg):
-    nvalues = list(i for i in values[:3])
+    nvalues = list(values[:3])
     nvalues[primaryValues[0]] = values[primaryValues[1]]
     nvalues[primaryValues[1]] = values[primaryValues[0]]
-    newimg.putpixel(coords,tuple(i for i in nvalues))
+    newimg.putpixel(coords,tuple(nvalues))
 
   newimg = perPixelAct(img,swap)
   print('Filtre appliqué.')
@@ -267,9 +276,8 @@ def imageSwap(img,primaryValues) :
   return newimg
 
 def imageColorReplace(img,colors,proximity) :
-  if img == None: 
-    print("Aucune image chargée.")
-    return img
+  imgCheck(img)
+
   if proximity == None:
     proximity = 0
 
@@ -290,6 +298,10 @@ def imageColorReplace(img,colors,proximity) :
   return newimg
 
 ## Helpers 
+def imgCheck(img):
+  if img == None:
+    raise Exception("Aucune image chargée.")
+
 def perPixelAct(img,pixelFunction):
   (w,h) = img.size
   newimg = pimg.new("RGB",(w,h))
@@ -301,21 +313,22 @@ def perPixelAct(img,pixelFunction):
   return newimg
 
 def getColor(color) :
-  if color[0] == "#" :
-    rgb = tuple(int(i,16) for i in (color[1:3],color[3:5],color[5:7]));
-  else :
-    rgb = tuple(int(i) for i in color.split(",")[:3])
-  return rgb
+  if color[0] == "#" and len(color) == 7:
+    return tuple(int(i,16) for i in (color[1:3],color[3:5],color[5:7]));
+  elif len(color.split(",")) == 3:
+    return tuple(int(i) for i in color.split(",")[:3])
+  else:
+    raise Exception(f'Invalid color format: "{color}" (should be 255,255,255 or #ffffff)')
 
-## Liste de commandes
+## Liste de commandes -> reference vers fonction
 commandList = { "help" : printHelp, 
-"load" : loadImg, 
-"filter" : filterApply,
-"pick" : pickColor,
-"show" : showImg, 
-"clear" : clearImg, 
-"save" : fileSave,
-"credit" : printCredit}
+                "load" : loadImg, 
+                "filter" : filterApply,
+                "pick" : pickColor,
+                "show" : showImg, 
+                "clear" : clearImg, 
+                "save" : fileSave,
+                "credit" : printCredit}
 
 ## REPL (Read eval print loop) -> pour afficher la console du programme
 print("[Tip] Tappez `help` pour voir la liste des commandes disponibles disponibles.")
@@ -326,7 +339,11 @@ while True:
   cmdkeyword = cmdargs[0]
 
   if cmdkeyword in commandList :
-    commandList[cmdkeyword](cmdargs[1:])
+    try: 
+      commandList[cmdkeyword](cmdargs[1:])
+    except Exception as error:
+      print(f'[Err] {cmdkeyword}: {error}')
+      
   elif cmdkeyword == "exit":
     commandList["clear"](None)
     print("Aurevoir !")
